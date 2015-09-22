51c9e9122929057727ce8834187af5f7e1853743
wing1995 <1130384613@qq.com>
2015-09-22 10:57:32 +0800

    the second lab

--- micblog/app/views.py
+++ micblog/app/views.py
@@ -1,4 +1,5 @@
-from flask import render_template
+from flask import render_template, flash, redirect
+from forms import LoginForm
 from app import app
 
 @app.route('/')
@@ -19,3 +20,15 @@ def index():
 				title = 'Home',
 				user = user,
 				posts = posts)
+
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+	form = LoginForm()
+	if form.validate_on_submit():
+		flash('Login requested for Name:' + form.name.data)
+		flash('passwd:' + str(form.password.data))
+		flash('remember_me:' + str(form.remember_me.data))
+		return redirect('/index')
+	return render_template('login.html',
+				title = 'Sign In',
+				form = form)
